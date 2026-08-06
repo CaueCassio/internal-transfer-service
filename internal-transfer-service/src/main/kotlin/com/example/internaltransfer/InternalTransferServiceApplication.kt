8b162e18323e6f3eb7c10c1fb83be4354ab2deb8
CaueCassio <cassio3620@gmail.com>
2026-08-06 19:13:31 -0300

    feat: init project

---
+++ internal-transfer-service/src/main/kotlin/com/example/internaltransfer/InternalTransferServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.internaltransfer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InternalTransferServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<InternalTransferServiceApplication>(*args)
+}
